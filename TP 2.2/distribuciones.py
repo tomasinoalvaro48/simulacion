f83b622923f9b1c36979d3ce11506178aceb3f4f
Tomas <tomas.aguilera.27@gmail.com>
2026-06-08 23:00:34 -0300

    2.2

--- TP 2.2/distribuciones.py
+++ TP 2.2/distribuciones.py
@@ -3,97 +3,6 @@ import numpy as np
 import random
 import math
 
-
-
-# Distribucion uniforme
-# f(x) =1/b-a para a <= x <= b
-'''
-def uniforme(a=0, b=1, candidatos_totales=500):
-
-
-    # Definimos los límites (rango finito a <= x <= b)
-    
-
-    # Aquí vamos a ir guardando los números que pasen la prueba
-    dist_uniforme_rechazo = []
-
-    for _ in range(candidatos_totales):
-        
-        # 1. Generamos la coordenada paso a paso
-        r1 = random.random()  
-        r2 = random.random()  
-        
-        # 2. Transformamos r1 para que caiga en nuestro rango de 0 a 1
-        x_candidato = a + (b - a) * r1
-        
-        # 3. Definimos la altura del techo de la curva
-        techo = 1.0
-        
-        # 4. El Filtro (Criterio de Aceptación/Rechazo)
-        if r2 <= techo:
-            dist_uniforme_rechazo.append((x_candidato, r1))
-        else:
-            pass
-
-  
-
-
-def distribucion_normal(a = -5, b = 5, candidatos_totales=500):
-    dist_normal_rechazo = []
-
-    for _ in range(candidatos_totales):
-        # Generamos un número aleatorio de la distribución normal
-        r_1 = random.random()  # Candidato (eje X)
-        r_2 = random.random()  # Juez (eje Y)
-
-        x_candidato = a + (b - a) * r_1
-
-        techo_en_x = math.exp(-0.5 * (x_candidato ** 2))
-            
-        # 5. El Filtro (Criterio de Aceptación/Rechazo)
-        if r_2 <= techo_en_x:
-            dist_normal_rechazo.append(x_candidato)
-        else:
-            pass
-        
-    print(f"De {candidatos_totales} intentos, se aceptaron {len(dist_normal_rechazo)} números normales. Porcentaje de aceptación: {len(dist_normal_rechazo) / candidatos_totales * 100:.2f}%")
-
- ''' 
-def distribucion_exponencial(lambd, candidatos_totales=500):
-    a=0
-    b=10
-    dist_exponencial_rechazo = []
-
-    for _ in range(candidatos_totales):
-
-        r_1 = random.random()
-        r_2 = random.random()
-
-        x_candidato = a + (b - a) * r_1
-
-        techo_en_x = (1/lambd)* lambd * math.exp(-lambd * x_candidato)
-
-        if r_2 <= techo_en_x:
-            dist_exponencial_rechazo.append(x_candidato)
-        else:
-            pass
-
-    print(f"De {candidatos_totales} intentos, se aceptaron {len(dist_exponencial_rechazo)} números exponenciales. Porcentaje de aceptación: {len(dist_exponencial_rechazo) / candidatos_totales * 100:.2f}%")
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
 def grafico_uniforme_rechazo(a=0, b=10, candidatos_totales=500):
     # Listas separadas para guardar las coordenadas de los buenos y los malos
     aceptados_x = []
@@ -105,7 +14,7 @@ def grafico_uniforme_rechazo(a=0, b=10, candidatos_totales=500):
     for _ in range(candidatos_totales):
         r1 = random.random()  # Eje X (0 a 1)
         
-        dardo_y = random.random() 
+        r2 = random.random() 
         
         # Transformamos r1 al rango [a, b]
         x_candidato = a + (b - a) * r1
@@ -114,12 +23,14 @@ def grafico_uniforme_rechazo(a=0, b=10, candidatos_totales=500):
         techo = 1.0
         
         # Filtro de Aceptación/Rechazo
-        if dardo_y <= techo:
+        if r2 <= techo:
             aceptados_x.append(x_candidato)
-            aceptados_y.append(dardo_y)
+            aceptados_y.append(r2)
         else:
             rechazados_x.append(x_candidato)
-            rechazados_y.append(dardo_y)
+            rechazados_y.append(r2)
+
+    print(f"De {candidatos_totales} intentos, se aceptaron {len(aceptados_x)} números uniformes. Porcentaje de aceptación: {len(aceptados_x) / candidatos_totales * 100:.2f}%")
 
     # --- GRÁFICO ---
     plt.figure(figsize=(10, 6))
@@ -170,7 +81,7 @@ def grafico_normal_rechazo(a=-5, b=5, candidatos_totales=500):
             rechazados_x.append(x_candidato)
             rechazados_y.append(r2) 
 
-
+    print(f"De {candidatos_totales} intentos, se aceptaron {len(aceptados_x)} números normales. Porcentaje de aceptación: {len(aceptados_x) / candidatos_totales * 100:.2f}%")
 
     # --- GRÁFICO ---
     plt.figure(figsize=(10, 6))
@@ -198,6 +109,17 @@ def grafico_normal_rechazo(a=-5, b=5, candidatos_totales=500):
     plt.show()
 
 grafico_normal_rechazo()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -253,6 +175,15 @@ def grafico_exponencial_rechazo(lambd=1, a=0, b=10, candidatos_totales=500):
     plt.show()
 
 grafico_exponencial_rechazo()
+
+
+
+
+
+
+
+
+
 
 
 
